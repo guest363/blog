0d5ef5b1ef811d8250a5c82b04c263f6c82440b1
Oleg Moiseev <guest363@gmail.com>
2020-05-09 23:55:46 +0300

    fix share

--- assets/js/share.js
+++ assets/js/share.js
@@ -1,12 +1,14 @@
-if(navigator.share) {
-    const shareButton = document.getElementById('share');
-    const shareText = shareButton.getAttribute('data-shareText');
-    const shareUrl = shareButton.getAttribute('href');
-navigator.share({
-    title: 'Блог Owl-shaker',
-    text: shareText,
-    url: shareUrl
-}).then(() => {
+const shareButton = document.getElementById('share');
+const shareText = shareButton.getAttribute('data-shareText');
+const shareUrl = shareButton.getAttribute('href');
+shareButton.addEventListener('click', function () {
+    if (navigator.share) {
+        navigator.share({
+            title: 'Блог Owl-shaker',
+            text: shareText,
+            url: shareUrl
+        }).then(() => {
 
-})
+        })
+    }
 }
